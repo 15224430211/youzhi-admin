9df89948863ddb8cd0b1e1ff6c9ae936479c233e
Dr.Lemon <512111519@qq.com>
2020-07-16 14:39:58 +0800

    ts,router,axios,vuex

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+// import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+// import * as d3 from 'd3';
 
 Vue.config.productionTip = false
 
